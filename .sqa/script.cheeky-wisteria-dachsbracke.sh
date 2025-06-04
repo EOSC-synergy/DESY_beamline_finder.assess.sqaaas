aa20ec9b7c4eedbdc3f7e92bc95111215dcabc04
Pablo Orviz <orviz@ifca.unican.es>
2025-06-04 09:44:49 +0200

    Add JePL file structure

---
+++ .sqa/script.cheeky-wisteria-dachsbracke.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.desy.de/melanie.nentwich/DESY_beamline_finder &&
+    flake8 .
+)
